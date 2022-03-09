9de046c5b68e71692f62945c2cb5d7de4d1cd51d
thrillim <hauniuni23@gmail.com>
2022-03-09 12:49:17 +0700

    new blank window pop-up

--- nonogram/main.cpp
+++ nonogram/main.cpp
@@ -1,23 +1,66 @@
-//
-//  main.cpp
-//  nonogram
-//
-//  Created by Hoang Thu Ha on 25/02/2022.
-//
 
 #include <iostream>
 #include <SDL2/SDL.h>
 
-using namespace std;
+//***********************************************
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
-const string WINDOW_TITLE = "Nonogram";
+const int SCREEN_WIDTH = 960;
+const int SCREEN_HEIGHT = 640;
+
+//***********************************************
 
 
 int main(int argc, char* argv[])
 {
-    cout << "Hello Nonogram" << endl;
+    SDL_Window* window = nullptr; //The window we'll be rendering to
+    SDL_Surface* windowSurface = nullptr; //The surface contained by the window
+    
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) //Initialize SDL
+    {
+        std::cout << "SDL could not initialize! SDL_Error: " <<  SDL_GetError() << std::endl;
+    }
+    else
+    {
+        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if(window == NULL)
+        {
+            std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        }
+        else
+        {
+            windowSurface = SDL_GetWindowSurface(window);
+            
+            //Fill the surface white
+            SDL_FillRect( windowSurface, NULL, SDL_MapRGB( windowSurface->format, 0xFF, 0xFF, 0xFF ) );
+                        
+            //Update the surface
+            SDL_UpdateWindowSurface( window );
+                
+            //-----------pop-up-fixed---------------------
+            SDL_UpdateWindowSurface(window);
+            bool is_running = true;
+            SDL_Event event;
+            while (is_running)
+            {
+                while (SDL_PollEvent(&event))
+                {
+                    if (event.type == SDL_QUIT)
+                    {
+                        is_running = false;
+                    }
+                }
+                SDL_Delay(16);
+            }
+            //--------------------------------------------
+        }
+    }
+    
+    SDL_DestroyWindow(window); //free memory
+    SDL_Quit();
     return 0;
 }
 
+//***********************************************
+
+
+//***********************************************
