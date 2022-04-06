024430f8e9075fb15ffc0b2f08970c8154c6dd30
thrillim <hauniuni23@gmail.com>
2022-04-06 18:10:14 +0700

    modify to be good looking

--- nonogram/SDL_Utils.hpp
+++ nonogram/SDL_Utils.hpp
@@ -5,23 +5,25 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
-void logSDLError(std::ostream& os,
-                 const std::string &msg,
-                 bool fatal);
+void logSDLError        (std::ostream& os,
+                         const std::string &msg,
+                         bool fatal);
 
-void initSDL(SDL_Window* &window,
-             SDL_Renderer* &renderer,
-             int screenWidth, int screenHeight,
-             const char* windowTitle);
+void initSDL            (SDL_Window* &window,
+                         SDL_Renderer* &renderer,
+                         int screenWidth, int screenHeight,
+                         const char* windowTitle);
 
-void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
+void quitSDL            (SDL_Window* window, SDL_Renderer* renderer);
 
 void waitUntilKeyPressed();
 
 SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren);
 
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y);
+void renderTexture      (SDL_Texture *tex, SDL_Renderer *ren, int x, int y);
 
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int h);
+void renderTexture      (SDL_Texture *tex, SDL_Renderer *ren,
+                         int x, int y,
+                         int w, int h);
 
 #endif /* SDL_Utils_hpp */
