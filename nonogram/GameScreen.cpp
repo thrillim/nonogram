29ef156289aa50a94c35c5dca3491f1ac0ba9abc
thrillim <hauniuni23@gmail.com>
2022-04-14 02:06:32 +0700

    load levels texture

--- nonogram/GameScreen.cpp
+++ nonogram/GameScreen.cpp
@@ -5,20 +5,30 @@
 
 const char WINDOW_TITLE[] = "Nonogram";
 const int SCREEN_WIDTH=960, SCREEN_HEIGHT=640;
-const int TABLE_X0=214, TABLE_Y0=53, TABLE_X1=748, TABLE_Y1=587;
-const int TABLE_ROW_NUM=13, TABLE_COL_NUM=13;
+const int MENU_PAGE = 2;
 
 SDL_Window* window;
 SDL_Renderer* renderer;
 
-SDL_Texture *mainMenu, *playGround;
+SDL_Texture *mainMenu[MENU_PAGE+1], *playGround;
 SDL_Texture *dot, *fill, *achive;
 SDL_Texture *numberOn[MAX_NUM+1], *numberOff[MAX_NUM+1];
 
 void load_SDL_And_Images()
 {
     initSDL(window, renderer, SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
-    mainMenu = loadTexture("/Users/haht/CodeSpace/Cpp/__LTNC__/XCode/nonogram/nonogram/assets/main.png", renderer);
+    bool is_load_main_failed = false;
+    for (int i=1; i<=MENU_PAGE; i++)
+    {
+        mainMenu[i] = loadTexture("/Users/haht/CodeSpace/Cpp/__LTNC__/XCode/nonogram/nonogram/assets/main" +std::to_string(i) + ".png", renderer);
+        
+        if (mainMenu[i] == nullptr)
+        {
+            is_load_main_failed = true;
+            break;
+        }
+    }
+
     playGround = loadTexture("/Users/haht/CodeSpace/Cpp/__LTNC__/XCode/nonogram/nonogram/assets/playGround.png", renderer);
     dot = loadTexture("/Users/haht/CodeSpace/Cpp/__LTNC__/XCode/nonogram/nonogram/assets/dot.png", renderer);
     fill = loadTexture("/Users/haht/CodeSpace/Cpp/__LTNC__/XCode/nonogram/nonogram/assets/fill.png", renderer);
@@ -45,7 +55,7 @@ void load_SDL_And_Images()
         }
     }
     
-    if (mainMenu == nullptr || playGround == nullptr || dot == nullptr || fill == nullptr || achive == nullptr || is_load_num_on_failed == true || is_load_num_off_failed == true)
+    if (playGround == nullptr || dot == nullptr || fill == nullptr || achive == nullptr || is_load_num_on_failed == true || is_load_num_off_failed == true || is_load_main_failed == true)
     {
         unload_SDL_And_Images();
         exit(1);
@@ -54,11 +64,14 @@ void load_SDL_And_Images()
 
 void unload_SDL_And_Images()
 {
-    SDL_DestroyTexture(mainMenu);
     SDL_DestroyTexture(playGround);
     SDL_DestroyTexture(fill);
     SDL_DestroyTexture(dot);
     SDL_DestroyTexture(achive);
+    for (int i = 0; i <= MENU_PAGE; i++)
+    {
+        SDL_DestroyTexture(mainMenu[i]);
+    }
     for (int i = 0; i <= MAX_NUM; i++)
     {
         SDL_DestroyTexture(numberOn[i]);
@@ -69,7 +82,7 @@ void unload_SDL_And_Images()
 
 void showMainMenu()
 {
-    renderTexture(mainMenu, renderer, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    renderTexture(mainMenu[1], renderer, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 }
 
 void renderScreen()
